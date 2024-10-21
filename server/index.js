8e30362d1b4d0fc3ab6222cb45262a02e3f04bbd
Fahim <fahim@nextsolutionlab.com>
2024-10-21 10:05:42 +0600

    deploying backend again

--- server/index.js
+++ server/index.js
@@ -17,11 +17,11 @@ const { port } = keys;
 
 const app = express();
 const corsOptions = {
-  origin: 'http://localhost:8070', // Replace with your client-side domain
-  credentials: true,
+  // origin: 'http://localhost:8070', // Replace with your client-side domain
+  // credentials: true,
 };
-app.use(cors(corsOptions));
-app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors("*"));
 
 
 
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(express.json());
-app.use(cors("http://localhost:8070"))
+// app.use(cors("http://localhost:8070"))
 app.use(morgan("dev"))
 
 
@@ -37,26 +37,26 @@ app.use(morgan("dev"))
 
 
 
-app.use(function (req, res, next) {
-  // console.log("loggg", req.url);
+// app.use(function (req, res, next) {
+//   // console.log("loggg", req.url);
 
-  // Website you wish to allow to connect
+//   // Website you wish to allow to connect
 
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost:8070");
-  // res.setHeader('Content-Security-Policy', 'script-src http://localhost:3000')
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, delete');
+//   res.setHeader('Access-Control-Allow-Origin', "http://localhost:8070");
+//   // res.setHeader('Content-Security-Policy', 'script-src http://localhost:3000')
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, delete');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,Accept,filename');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,Accept,filename');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 app.use(router)
 
